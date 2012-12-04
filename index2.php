c6a52868b928cd7b893189cc7cfe7da9cd8a4b9b
jearly <earlyster@gmail.com>
2012-12-04 15:00:05 -0800

    fixing broken link

--- index2.php
+++ index2.php
@@ -1,6 +1,6 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	$pageTitle 		= "VJET JavaScript IDE";
-	$html = file_get_contents('_index.html');
+	$html = file_get_contents('pages/_index.html');
 	
 	# Generate the web page
 	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
