3c33eb2c6b1511a8529be0a28af1f7c0d16df2fd
jearly <earlyster@gmail.com>
2013-02-11 11:31:28 -0800

    fixing year

--- download/index.php
+++ download/index.php
@@ -18,7 +18,7 @@
 <h2>$pageTitle</h2>
 <p>All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
 
-<h3>Installing latest release 0.9.1 (Released 2/8/2012) </h3>
+<h3>Installing latest release 0.9.1 (Released 2/8/2013) </h3>
 <p><b>Update site:</b> http://download.eclipse.org/vjet/updates-0.9
 
 <a href="http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=46875" title="Drag and drop into a running Eclipse Indigo workspace to install VJET JavaScript IDE">
